8bc9f5096cd9af8738a7b8dab0e8446ecbd5532d
Bitfroest <fabian.schurig.94@gmail.com>
2017-07-04 19:01:22 +0200

    added database, added more commands, added ratelimit for calls

--- aws_api.py
+++ aws_api.py
@@ -17,8 +17,18 @@ import config
 
 from bs4 import BeautifulSoup
 
-def checkOffers(itemId):
+import re
+
+import time
+
+from ratelimit import *
+
+def checkOffers(itemIdStr, responseGroup):
     params = {}
+    
+    itemId = validateItemIds(itemIdStr)
+    if not itemId:
+        return
 
     params['AWSAccessKeyId'] = config.ACCESS_KEY
     params['Service'] = 'AWSECommerceService'
@@ -26,7 +36,7 @@ def checkOffers(itemId):
     params['AssociateTag'] = config.ASOC_TAG
     params['Timestamp'] = strftime("%Y-%m-%dT%H:%M:%SZ", gmtime())
     params['Operation'] = 'ItemLookup'
-    params['ResponseGroup'] = 'Offers' #OfferSummary
+    params['ResponseGroup'] = responseGroup #'OfferSummary' #Offers
     params['IdType'] = 'ASIN'
     params['ItemId'] = itemId
     
@@ -35,25 +45,60 @@ def checkOffers(itemId):
     
     #print(response.decode("utf-8"))
     result = {}
+    result['items'] = []
+    
+    ts = time.time()
+    timestamp = datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
     
     b = BeautifulSoup(response, 'lxml')
     #print(b.prettify())
-    lowestPrice = b.find('lowestnewprice')
-    if (lowestPrice is not None):
-        lowestFormattedPrice = lowestPrice.formattedprice.string
-        result['LowestNewPrice'] = lowestFormattedPrice
+    items = b.find_all('item')
+    for item in items:
+        itemResult = {}
+        itemResult['prices'] = []
+        
+        lowestPrice = item.find('lowestnewprice')
+        if lowestPrice is not None:
+            price = {} # Condition, Amount, CreatedAt, CurrencyCode
+            price['Amount'] = lowestPrice.amount.string
+            price['Condition'] = 0
+            price['CreatedAt'] = timestamp
+            price['CurrencyCode'] = lowestPrice.currencycode.string
+            itemResult['prices'].append(price)
+            
+        
+        lowestUsedPrice = item.find('lowestusedprice')
+        if lowestUsedPrice is not None:
+            price = {} # Condition, Amount, CreatedAt, CurrencyCode
+            price['Amount'] = lowestUsedPrice.amount.string
+            price['Condition'] = 1
+            price['CreatedAt'] = timestamp
+            price['CurrencyCode'] = lowestUsedPrice.currencycode.string
+            itemResult['prices'].append(price)
+            print(price['Amount'])
+        
+        link = item.find('moreoffersurl')
+        if link is not None:
+            linkStr = link.string
+            itemResult['MoreOffersUrl'] = linkStr
+        if item.asin is not None:
+            itemResult['ASIN'] = item.asin.string
+        result['items'].append(itemResult)
+    print(result)
+    return result
+
+def validateItemIds(itemIdStr):
+    itemIds = itemIdStr.split(',')
+    valid = []
+    for item in itemIds:
+        check = isValidASIN(item)
+        if check is not None:
+            valid.append(item)
+    return ','.join(valid)
     
-    lowestUsedPrice = b.find('lowestusedprice')
-    if (lowestUsedPrice is not None):
-        usedFormattedPrice = lowestUsedPrice.formattedprice.string
-        result['LowestUsedPrice'] = usedFormattedPrice
-    
-    link = b.find('moreoffersurl')
-    linkStr = link.string
-    result['MoreOffersUrl'] = linkStr
-    result['itemId'] = itemId
-    
-    return result   
+def isValidASIN(itemId):
+    pattern = re.compile("^B\d{2}\w{7}|\d{9}(X|\d)$")
+    return pattern.match(itemId)
     
     
 def signateUrl(params):
@@ -74,10 +119,13 @@ def signateUrl(params):
     signature = quote(b64encode(hash.digest()))
     url = 'http://%s/onca/xml?%s&Signature=%s' % (config.HOST, args, signature)
     return url
-    
+
+@rate_limited(1)    
 def sendRequest(url, userAgent):
     try:
         response = requests.get(url, stream=True, headers={'User-Agent': userAgent})
         return response.content
-    except:
+    except Exception:
         return False
+        
+#print(checkOffers(config.ASIN))
--- checker.py
+++ checker.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import time
-from datetime import datetime
+from datetime import datetime,timedelta
 import config
 import telegram
 import logging
@@ -12,11 +12,24 @@ from random import randint
 
 from telegram.ext import Updater
 
-import aws_api as aws
+import aws_api
+
+import sqlite3
+
+conn = sqlite3.connect('awd.db', check_same_thread=False)
+c = conn.cursor()
 
 updater = Updater(token=config.AUTH_TOKEN)
 
 counter = 0
+
+def terminate(signal, frame):
+    print("received SIGINT, exit...")
+    updater.stop()
+    quit()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, terminate)
 
 telegramBot = telegram.Bot(token=config.AUTH_TOKEN)
 print(telegramBot.getMe())
@@ -26,51 +39,154 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    t = (str(update.message.chat_id),)
+    c.execute('SELECT * FROM Users WHERE chatID=?', t)
+    row = c.fetchone()
+    print(row)
+    if row is None:
+        c.execute('INSERT INTO Users (chatID, isAdmin) VALUES (?,?)',(str(update.message.chat_id),0,))
+        conn.commit()
+        print('Inserted new user with chat id '+str(t))
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
+def addItem(bot, update, args):
+    print(args)
+    if (len(args) == 1):
+        t = (str(update.message.chat_id),)
+        c.execute('SELECT id FROM Users WHERE chatID=?', t)
+        userId = c.fetchone()
+        
+        asinStr = aws_api.validateItemIds(args[0])
+        asins = asinStr.split(',')
+        noInfo = []
+        for asin in asins:
+            c.execute('SELECT * FROM Items WHERE asin=?', (str(asin),))
+            row = c.fetchone()
+            if row is None:
+                noInfo.append(asin)
+            else:
+                c.execute('SELECT * FROM Track WHERE UserID=? and ItemID=?', (userId[0],row[0],))
+                if c.fetchone() is None:
+                    c.execute('INSERT INTO Track (ItemID, UserID, TargetAmount) VALUES (?,?,?)',(row[0],userId[0],0,))
+                    conn.commit()
+        alreadyTracked = []
+        c.execute('SELECT ItemID FROM Track WHERE UserID=?', userId)
+        rows = c.fetchall()
+        print(rows)
+        for row in rows:
+            alreadyTracked.append(row[0])
+            print(row[0])
+        if len(noInfo) > 0 and len(noInfo) <= 10:    
+            items = aws_api.checkOffers(','.join(noInfo),'Offers')
+            for item in items['items']:
+                if 'ASIN' in item and 'MoreOffersUrl' in item:
+                    c.execute('INSERT INTO Items (asin, url) VALUES (?,?)',(str(item['ASIN']),str(item['MoreOffersUrl']),))
+                    conn.commit()
+                    c.execute('SELECT id FROM Items WHERE asin=?', (str(item['ASIN']),))
+                    row = c.fetchone()
+                    print(row)
+                    for price in item['prices']:
+                        c.execute('INSERT INTO Prices (ItemID, Condition, Amount, CreatedAt, CurrencyCode) VALUES (?,?,?,?,?)',(row[0],price['Condition'],price['Amount'],price['CreatedAt'],price['CurrencyCode'],))
+                        conn.commit()
+                    if row[0] not in alreadyTracked:
+                        c.execute('INSERT INTO Track (ItemID, UserID, TargetAmount) VALUES (?,?,?)',(row[0],userId[0],0,))
+                        conn.commit()
+                else:
+                    print('Fail')
+            bot.send_message(chat_id=update.message.chat_id, text= 'successfully added '+str(len(noInfo))+' items', parse_mode=telegram.ParseMode.MARKDOWN)
+        print(update.message.chat_id)
+    
+addItem_handler = CommandHandler('add', addItem, pass_args=True)
+dispatcher.add_handler(addItem_handler)
+
 def check(bot, update, args):
     print(args)
     if (len(args) == 1):
-        info = aws.checkOffers(args[0])
-        bot.send_message(chat_id=update.message.chat_id, text= 'Neu: ' + info['LowestNewPrice'] + ' Gebraucht: ' + info['LowestUsedPrice'] + ' [link](' + info['MoreOffersUrl'] + ')', parse_mode=telegram.ParseMode.MARKDOWN)
+        asinStr = aws_api.validateItemIds(args[0])
+        asins = asinStr.split(',')
+        for asin in asins:
+            c.execute('SELECT * FROM Items WHERE asin=?', (str(asin),))
+            row = c.fetchone()
+            bot.send_message(chat_id=update.message.chat_id, text= row, parse_mode=telegram.ParseMode.MARKDOWN)        
         print(update.message.chat_id)
     
 check_handler = CommandHandler('check', check, pass_args=True)
 dispatcher.add_handler(check_handler)
 
+def info(bot, update):
+    c.execute('SELECT Max(Prices.CreatedAt) FROM Prices')
+    maxTime = c.fetchone()
+    queryTime = (datetime.strptime(maxTime[0], '%Y-%m-%d %H:%M:%S') - timedelta(seconds=120)).strftime('%Y-%m-%d %H:%M:%S')
+    print(maxTime)
+    print(queryTime)
+    c.execute('SELECT Distinct chatID,url,asin,Amount,Condition,CurrencyCode FROM Prices INNER JOIN Items ON Prices.ItemID=Items.id INNER JOIN Track ON Items.id=Track.ItemID INNER JOIN Users On Users.id=Track.UserID Where Prices.CreatedAt>? and Users.chatID=?',(queryTime,update.message.chat_id,))
+    rows = c.fetchall()
+    print(rows)
+    for row in rows:
+        bot.send_message(chat_id=update.message.chat_id, text= 'Item '+'ASIN: '+row[2]+' Preis: '+str(row[3]/100)+' '+row[5]+' Zustand: '+('Gebraucht' if row[4] == 1 else 'Neu')+' [link]('+row[1]+')', parse_mode=telegram.ParseMode.MARKDOWN)      
+    print(update.message.chat_id)
+    
+info_handler = CommandHandler('info', info)
+dispatcher.add_handler(info_handler)
+
 updater.start_polling()
 
-prices = []
+def updatePrices(itemId):
+    info = aws_api.checkOffers(itemId, responseGroup = 'OfferSummary')
+    updated = False
+    for item in info['items']:
+        if 'ASIN' in item and 'prices' in item:
+            for price in item['prices']:
+                c.execute('SELECT id FROM Items WHERE asin=?', (str(item['ASIN']),))
+                row = c.fetchone()
+                c.execute('INSERT INTO Prices (ItemID, Condition, Amount, CreatedAt, CurrencyCode) VALUES (?,?,?,?,?)',(row[0],price['Condition'],price['Amount'],price['CreatedAt'],price['CurrencyCode'],))
+                conn.commit()
+                updated = True
+    return updated
 
-def checkProduct(itemId):
-    info = aws.checkOffers(itemId)
-    if info['LowestUsedPrice'] in prices:
-        return True
-    else:
-        prices.append(info['LowestUsedPrice'])
-        telegramBot.send_message(chat_id=config.CHAT_ID, text= 'ASIN: ' + info['itemId'] + ' Neu: ' + info['LowestNewPrice'] + ' Gebraucht: ' + info['LowestUsedPrice'] + ' [link](' + info['MoreOffersUrl'] + ')', parse_mode=telegram.ParseMode.MARKDOWN)
-        print(info['LowestUsedPrice'])
-    return True
 
 if __name__ == "__main__":
     while True:
-        if counter > 60:
-            del prices[:]
-            telegramBot.send_message(chat_id=config.CHAT_ID, text= 'deleted cache')
-        if checkProduct(config.ASIN) == True:
-            print('sleeping for 60 seconds.')
-            print(str(datetime.now()))
-            time.sleep(60)
-            print()
+        c.execute('SELECT Max(Prices.CreatedAt) FROM Prices')
+        maxTime = c.fetchone()
+        if maxTime[0] < (datetime.fromtimestamp(time.time()) - timedelta(seconds=61)).strftime('%Y-%m-%d %H:%M:%S'):
+            print('old data')
+        c.execute('SELECT Distinct asin FROM Items INNER JOIN Track ON Items.id=Track.ItemID')
+        rows = c.fetchall()
+        print(rows)
+        counter = 0
+        asins = []
+        length = len(rows)
+        for row in rows:
             counter = counter + 1
-        
-        else:
-            print('Sorry! Nothing found. Sleeping for 60 seconds.')
-            print(str(datetime.now()))
-            time.sleep(60)
-            print()
+            length = length - 1
+            asins.append(row[0])
+            if counter > 8 or length == 0:
+                counter = 0
+                status = updatePrices(','.join(asins))
+                print('updating following products: '+','.join(asins))
+                print('update '+str(status))
+        ts = time.time()
+        timestamp = (datetime.fromtimestamp(ts) - timedelta(seconds=61)).strftime('%Y-%m-%d %H:%M:%S')
+        print('Timestamp: '+timestamp)
+        c.execute('SELECT * FROM Prices Where Prices.CreatedAt>? and Prices.Condition=1 group by Prices.ItemID,Prices.Amount order by Prices.CreatedAt ASC',(timestamp,))
+        rows = c.fetchall()
+        itemIds = []
+        for row in rows:
+            if row[1] in itemIds:
+                print(row)
+                c.execute('SELECT Distinct chatID,url,asin,Amount,Condition,CurrencyCode FROM Prices INNER JOIN Items ON Prices.ItemID=Items.id INNER JOIN Track ON Items.id=Track.ItemID INNER JOIN Users On Users.id=Track.UserID Where Prices.ItemID=? and Prices.Condition=1 and Prices.CreatedAt= (SELECT Max(Prices.CreatedAt) FROM Prices)',(row[1],))
+                rowsN = c.fetchall()
+                print(rowsN)
+                for rowN in rowsN:
+                    telegramBot.send_message(chat_id=rowN[0], text= 'Preisänderung '+'ASIN: '+rowN[2]+' Preis: '+str(rowN[3]/100)+' '+rowN[5]+' Zustand: '+('Gebraucht' if rowN[4] == 1 else 'Neu')+' [link]('+rowN[1]+')', parse_mode=telegram.ParseMode.MARKDOWN)
+            else:
+                itemIds.append(row[1])
+        print(str(datetime.now()))
+        time.sleep(30)
+        print()
 
 
 
